31a3a6761c9b07353164286963e4e4842cea7a9a
Sooyoung Park <sooyoung32@gmail.com>
2015-12-15 20:52:56 +0900

    first messages

---
+++ src/main/java/korea/hack/test/HackKoreaTestApplication.java
@@ -0,0 +1,12 @@
+package korea.hack.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackKoreaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackKoreaTestApplication.class, args);
+    }
+}
